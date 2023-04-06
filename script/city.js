25193a6a8a23f17e9b6fa5e52e388a8593016bdc
Arif Ansari <arifansari729878@gmail.com>
2023-04-06 21:15:37 +0530

    weatherApp
    The weather application also provides atmospheric pressure, weather conditions, wind speed and humidity in different units

---
+++ script/city.js
@@ -0,0 +1,38 @@
+const key = "AyWMx25uXFnbN7GZ681kEmUQbbLyzgW2";
+const openkey = "ed3940f963ff4fb918275973ee72de8f";
+
+const getCity = async (city) => {
+    const request = await fetch(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${key}&q=${city}`);
+    const data = await request.json();
+    return data[0];
+}
+
+const getWeather = async (cityId) => {
+    const request = await fetch(`http://dataservice.accuweather.com/currentconditions/v1/${cityId}?apikey=${key}`)
+    const data = await request.json();
+    return data[0];
+}
+
+
+const getTimeDate = async () => {
+    const current = new Date();
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const month = months[current.getMonth()];
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const day = days[current.getDay()]
+    const time = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+    return dateTime = {
+        "day": day,
+        "date": current.getDate(),
+        "month": month,
+        "year": current.getFullYear(),
+        "time": time
+    }
+}
+
+const openweather = async (city) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}`;
+    const request = await fetch(url+`&appid=${openkey}`);
+    const data = await request.json();
+    return data;
+}
---
+++ script/weather.js
@@ -0,0 +1,65 @@
+const form = document.querySelector("form");
+const city = document.querySelector(".date-time");
+const backround = document.querySelector(".background-img img");
+const weatherIcon = document.querySelector(".weather-status");
+const Temp = document.querySelector(".tempreture-detail");
+const windSec = document.querySelector(".wind-detail");
+
+const getCityData = async (city) => {
+    const cityDetails = await getCity(city);
+    const weatherDetails = await getWeather(cityDetails.Key);
+    const openweatherdetails = await openweather(city);
+    const TimeDate = await getTimeDate()
+    return { cityDetails, weatherDetails, openweatherdetails, TimeDate };
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const userCity = form.cityName.value.trim();
+    console.log(userCity);
+    form.reset();
+
+    // Getting All Data
+    getCityData(userCity).then((data) => {
+        return updateUI(data);
+    }).catch((error) => {
+        console.log(error);
+    })
+
+});
+
+const updateUI = (data) => {
+    const { cityDetails, weatherDetails, openweatherdetails, TimeDate } = data
+    console.log(data)
+    
+    // city Date Time 
+    city.innerHTML = `
+    <h1>${data.cityDetails.LocalizedName}</h1>
+    <h2>${data.TimeDate.day}</h2>
+    <h3> ${data.TimeDate.month} ${data.TimeDate.date}, ${data.TimeDate.year} </h3>
+    <h4>${data.TimeDate.time}</h4>`
+
+    // Day Night Switch
+    const imgSrc = weatherDetails.IsDayTime ? 'image/day.jpg' : 'image/night.jpg';
+    backround.setAttribute('src', imgSrc);
+
+    // icon Switch 
+    weatherIcon.innerHTML = `
+    <h1>${weatherDetails.WeatherText}</h1>
+    <img src="./image/icons/${weatherDetails.WeatherIcon}.svg" alt="icon">
+    `
+
+    // Temp 
+    Temp.innerHTML = `
+    <h1>${weatherDetails.Temperature.Metric.Value}<span> &#8451;</span></h1> 
+    <h1>${weatherDetails.Temperature.Imperial.Value}<span> &#8457;</span></h1>
+    `
+    // Extra Details 
+    windSec.innerHTML=`
+    <p>Wind Speed <span> ${openweatherdetails.wind.speed} </span> Meter/Sec</p>
+    <p>Humidity <span> ${openweatherdetails.main.humidity} </span>% </p>
+    <p>Pressure <span> ${openweatherdetails.main.pressure} </span>hPa </p>
+    <p>Max Temp <span> ${openweatherdetails.main.temp_max} </span>Kelvin </p>
+    <p>Min Temp <span> ${openweatherdetails.main.temp_min} </span>Kelvin </p>
+    `
+}
